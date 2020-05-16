48b51228bcde83c9ccfe79f302040fa97bd4b22e
Deihim007 <deihim007@gmail.com>
2020-05-17 02:55:52 +0430

    Added timing.

--- stats/stats.py
+++ stats/stats.py
@@ -1,5 +1,6 @@
 from redbot.core import commands, checks
 from .ase import fetch_data
+import time
 
 class Stats(commands.Cog):
     """My custom cog"""
@@ -8,12 +9,31 @@ class Stats(commands.Cog):
     async def stats(self, ctx):
         """MTA:SA Stats!"""
         pass
+
     @stats.command()
     @commands.cooldown(1, 60, commands.BucketType.user)
+    @commands.guild_only()
     async def online(self, ctx):
         """Shows MTA's online players count"""
         total = 0
+        start = time.time()
+        end = time.time()
+        took = end - start
         servers = fetch_data()
         for server in servers:
             total += server.playersCount 
-        await ctx.channel.send("There are currently "+str(total)+" players online!")
+        await ctx.channel.send("There are currently "+str(total)+" players online! Timing: "+str(took))
+
+    @stats.command()
+    @commands.cooldown(1, 60, commands.BucketType.user)
+    @commands.guild_only()
+    async def top(self, ctx):
+        """Shows the servers with most players."""
+        top = ""
+        peak = 0
+        servers = fetch_data()
+        for server in servers:
+            if peak < server.playersCount:
+                peak = server.playersCount
+                top = server.serverName
+        await ctx.channel.send("`"+top+"` with "+str(peak)+" players!")
