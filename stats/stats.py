3733656c372ab042f18184f1100d6f70a2f160b7
Deihim007 <deihim007@gmail.com>
2020-05-17 04:23:51 +0430

    Added async and typing.

--- stats/stats.py
+++ stats/stats.py
@@ -1,9 +1,12 @@
 from redbot.core import commands, checks
-from .ase import fetch_data
-import time
+from .ase import MTAServerlist
+
+import aiohttp, asyncio, time
 
 class Stats(commands.Cog):
     """My custom cog"""
+    def __init__(self):
+        self.url = 'https://master.multitheftauto.com/ase/mta/'
 
     @commands.group()
     async def stats(self, ctx):
@@ -15,24 +18,31 @@ class Stats(commands.Cog):
     @commands.guild_only()
     async def online(self, ctx):
         """Shows MTA's online players count"""
-        total = 0
-        start = time.time()
-        servers = fetch_data()
-        took = time.time() - start
-        for server in servers:
-            total += server.playersCount 
-        await ctx.channel.send("There are currently "+str(total)+" players online! Timing: "+str(took))
+        async with ctx.typing():
+            total = 0
+            async with aiohttp.ClientSession() as session:
+                async with session.get(self.url) as response:
+                    ase = MTAServerlist()
+                    await ase.parse(await response.read())
+
+            for server in ase.servers:
+                total += server["players"] 
+            await ctx.channel.send("There are currently "+str(total)+" players online!")
 
     @stats.command()
     @commands.cooldown(1, 60, commands.BucketType.user)
     @commands.guild_only()
     async def top(self, ctx):
         """Shows the servers with most players."""
-        top = ""
-        peak = 0
-        servers = fetch_data()
-        for server in servers:
-            if peak < server.playersCount:
-                peak = server.playersCount
-                top = server.serverName
-        await ctx.channel.send("`"+top+"` with "+str(peak)+" players!")
+        async with ctx.typing():
+            top = ""
+            peak = 0
+            async with aiohttp.ClientSession() as session:
+                async with session.get(self.url) as response:
+                    ase = MTAServerlist()
+                    await ase.parse(await response.read())
+            for server in ase.servers:
+                if peak < server.playersCount:
+                    peak = server["players"] 
+                    top = server["name"] 
+            await ctx.channel.send("`"+top+"` with "+str(peak)+" players!")
